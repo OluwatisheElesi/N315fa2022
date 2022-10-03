9e3a57f77757020bc1c6305f5f7cb23e240c8b1b
OluwatisheElesi <tisheelesh.logic@gmail.com>
2022-10-03 15:37:38 -0400

    2

--- Homework/homework3/app/models/model.js
+++ Homework/homework3/app/models/model.js
@@ -1,12 +1,27 @@
 var homeContent = '<h1>Home</h1>';
-// var coursesContent = '<h1>Courses</h1>';
+var coursesContent = '<h1>Courses</h1>';
 var workshopsContent = '<h1>Workshops</h1>';
 var studentsContent = '<h1>Students</h1>';
 var blogContent = '<h1>Blog</h1>';
 
 export function changePage(pageID) {
-    let page = pageID + "Content";
-    $("app").hmtl(eval(page));
+    // let page = pageID + "Content";
+//    path
+$.get(`pages/${pageId}.html`, function (data) {
+
+    console.log(data);
+    
+    $("#app").html(data);
+
+}).fail((error)  => { 
+    if(error.status == "404") { 
+        alert("Page can not be found. Please check your url.")
+
+    }
+   console.log("error" , error.status);
+});
+
+    // $("#app").html(eval(page));
 }
     // $get('')
     // $(#appCredentialsC)
